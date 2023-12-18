9f585a4e75677659c664f6fb3d7ba6b5ce230c67
zhanglei <zhanglei@hjsoft.com.cn>
2023-12-18 18:36:59 +0800

    sync_blog插件

--- main.ts
+++ main.ts
@@ -1,81 +1,75 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
+
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface SyncCnblogSettings {
+	blog_url: string;
+	blog_id: string;
+	username: string;
+	password: string;
+
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: SyncCnblogSettings = {
+	blog_url: "https:/ /rpc.cnblogs.com/metaweblog/aaalei",
+	blog_id: "aaalei",
+	username: "2468341590@qq.com",
+	password: "5529D103516E0289554BD76D87CBABC72811A92F37AEA3ABE3B8266D3A1B5F9"
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class SyncCnblogPlugin extends Plugin {
+	settings: SyncCnblogSettings;
 
 	async onload() {
+		// 添加指令到ctrl+p的控制面板
+		this.addCommand({
+			id: "sync_cnblog",
+			name: "sync_cnblog",
+			callback: () => {
+				console.log("嗨你好!!");
+			},
+		});
+
+		// 注册指令到右键的文件菜单
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, file) => {
+				menu.addItem((item) => {
+					item
+						.setTitle("同步到博客园")
+						.setIcon("upload")
+						.onClick(async () => {
+							
+							
+							ajax(
+								{
+									method: 'GET',
+									url: file.vault.adapter.getBasePath()+"/" +file.path,
+									success: function(data){
+										//请求成功后执行该函数
+										console.log(data)//tom
+									}
+									
+								}
+							)
+							new Notice( file.vault.adapter.getBasePath()+"/" +file.path);
+							console.log(file.vault.adapter.getBasePath()+"/" +file.path)
+						});
+				});
+			}))
+
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
+		// 创建左侧图标, 点击时的响应事件
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			new Notice('Hello World!');
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
+
+
+		// 这添加了一个设置选项卡，以便用户可以配置插件的各个方面
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,43 +85,60 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: SyncCnblogPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SyncCnblogPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('blog_url')
+			.setDesc('博客园MetaWeblog访问地址')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('请输入你的参数')
+				.setValue(this.plugin.settings.blog_url)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.blog_url = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('blog_id')
+			.setDesc('博客园MetaWeblog访问地址后缀')
+			.addText(text => text
+				.setPlaceholder('请输入你的参数')
+				.setValue(this.plugin.settings.blog_id)
+				.onChange(async (value) => {
+					this.plugin.settings.blog_id = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('username')
+			.setDesc('MetaWeblog登录名')
+			.addText(text => text
+				.setPlaceholder('请输入你的参数')
+				.setValue(this.plugin.settings.username)
+				.onChange(async (value) => {
+					this.plugin.settings.username = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('password')
+			.setDesc('MetaWeblog访问令牌')
+			.addText(text => text
+				.setPlaceholder('请输入你的参数')
+				.setValue(this.plugin.settings.password)
+				.onChange(async (value) => {
+					this.plugin.settings.password = value;
 					await this.plugin.saveSettings();
 				}));
 	}
